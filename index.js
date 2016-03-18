c502d93ab0856b1e09001c011d0016eaa5b090ad
Howard Liang <liang42hao@users.noreply.github.com>
2016-03-18 11:13:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -238,6 +238,9 @@
    *  \n\
    */\n\
    \n\
+  #msg {    \n\
+      font: normal bold 42px/50px Andale Mono, monospace;\n\
+  }    \n\
   .typed-cursor{\n\
       opacity: 1;\n\
       -webkit-animation: blink 0.7s infinite;\n\
